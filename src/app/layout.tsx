7e9576d52283b6f7cd054009184d30ceb97d8f22
Emman Nipal <emnnipal@gmail.com>
2023-07-13 00:53:20 +0800

    fix: typo in detailers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import type { Metadata } from 'next';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "185 Detailer's Den",
+  title: '185 Detailers Den',
   description:
-    "185 Detailer's Den is a car detailing and car wash business located in Caloocan City, Philippines.",
+    '185 Detailers Den is a car detailing and car wash business located in Caloocan City, Philippines.',
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
