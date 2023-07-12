7fc701615893871088d6911a28b694cdbb22061c
Emman Nipal <emnnipal@gmail.com>
2023-07-13 00:28:50 +0800

    feat: add initial page for detailer's den

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  singleQuote: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 100,
+  semi: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
