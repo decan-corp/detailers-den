956108ee459759da985d99706bde64de4527ca26
Emman Nipal <emnnipal@gmail.com>
2023-11-03 18:59:45 +0800

    feat: add svg and update next js to v14
    - update next js to v14
    - setup import react svg

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const wordpressApiUrl = process.env.NEXT_PUBLIC_WORDPRESS_API_URL || '';
+const nextConfig = {
+  images: {
+    domains: [wordpressApiUrl.replace('https://', '')],
+  },
+  webpack: (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test?.test?.('.svg')
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: require.resolve('@svgr/webpack'),
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
