7ba8078e884025450a9d47339e981ee8936b94e9
Emman Nipal <emnnipal@gmail.com>
2023-11-04 00:33:18 +0800

    feat: add opengraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,15 @@ export const metadata: Metadata = {
   title: '185 Detailers Den',
   description:
     '185 Detailers Den is a car detailing and car wash business located in Caloocan City, Philippines.',
+  keywords: ['car wash', 'detailing', 'cleaning', 'caloocan', 'car'],
+  openGraph: {
+    title: '185 Detailers Den',
+    description:
+      '185 Detailers Den is a car detailing and car wash business located in Caloocan City, Philippines.',
+    url: process.env.VERCEL_URL,
+    siteName: '185 Detailers Den',
+    type: 'website',
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
