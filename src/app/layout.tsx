26e52983b48d03cd946bcb25e73710c9720bb844
Emman Nipal <emnnipal@gmail.com>
2023-11-04 00:46:26 +0800

    fix: incorrect canonical url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
     title: '185 Detailers Den',
     description:
       '185 Detailers Den is a car detailing and car wash business located in Caloocan City, Philippines.',
-    url: process.env.VERCEL_URL,
+    url: `https://${process.env.VERCEL_URL}`,
     siteName: '185 Detailers Den',
     type: 'website',
   },
