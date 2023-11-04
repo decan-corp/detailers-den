49a3c1d0daf9c956e8bc195bb20f64a05f0e080d
Emman Nipal <emnnipal@gmail.com>
2023-11-04 20:05:11 +0800

    feat: add loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,6 @@
+const Loading = () => (
+  <div className="flex h-screen items-start justify-center bg-white">
+    <span className="loading loading-ring loading-lg mt-36" />
+  </div>
+);
+export default Loading;
