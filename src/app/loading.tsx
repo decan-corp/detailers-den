8d69445d9100be34a85362db70f31b2b95f6a515
Emman Nipal <emnnipal@gmail.com>
2023-11-04 20:27:05 +0800

    chore: fix alignment of placeholder messages in mobile view

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => (
   <div className="flex h-screen items-start justify-center bg-white">
-    <span className="loading loading-ring loading-lg mt-36" />
+    <span className="loading loading-ring loading-lg mt-44" />
   </div>
 );
 export default Loading;
--- src/app/promos/page.tsx
+++ src/app/promos/page.tsx
@@ -3,8 +3,13 @@ import { bebasNeue } from 'src/utils/fonts';
 import { twJoin } from 'tailwind-merge';
 
 const Promos = () => (
-  <div className="flex h-screen items-start justify-center">
-    <div className={twJoin(bebasNeue.className, 'mt-60 text-3xl tracking-widest')}>
+  <div className="flex h-screen items-start justify-center px-10">
+    <div
+      className={twJoin(
+        bebasNeue.className,
+        'mt-60 text-center text-3xl leading-loose tracking-widest'
+      )}
+    >
       Exciting Content in Progress - Check Back Soon!
     </div>
   </div>
