4c212f1792eebf63c075582711fd40315343a022
Emman Nipal <emnnipal@gmail.com>
2023-11-15 14:05:23 +0800

    feat: add seeding script

---
+++ scripts/seedDatabase.ts
@@ -0,0 +1,28 @@
+// spell-checker:disable
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable no-console */
+
+import { serverEnv } from 'src/env/server';
+import { users } from 'src/schema';
+import { db } from 'src/utils/db';
+
+import bcrypt from 'bcryptjs';
+
+const seedDatabase = async () => {
+  // Create default user
+  const password = 'P@ssw0rd!23';
+  const saltedPassword = await bcrypt.hash(password, serverEnv.SALT_ROUNDS);
+  await db.insert(users).values({
+    email: 'emnnipal@gmail.com',
+    name: 'Emman',
+    password: saltedPassword,
+  });
+
+  console.log('Success seeding database');
+  process.exit(0);
+};
+
+seedDatabase().catch((err) => {
+  console.error('Failed to seed database', err);
+  process.exit(1);
+});
