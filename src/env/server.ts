6f859112213b569298e96189626e38be0c243c8a
Emman Nipal <emnnipal@gmail.com>
2023-11-15 14:07:54 +0800

    feat: add client and server env

--- src/env/server.ts
+++ src/env/server.ts
@@ -1,10 +1,15 @@
+import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
-const serverEnvSchema = z.object({
-  DB_HOST: z.string().min(1),
-  DB_USERNAME: z.string().min(1),
-  DB_PASSWORD: z.string().min(1),
-  DB_NAME: z.string().optional(),
+export const serverEnv = createEnv({
+  server: {
+    DB_HOST: z.string().min(1),
+    DB_USERNAME: z.string().min(1),
+    DB_PASSWORD: z.string().min(1),
+    DB_NAME: z.string().optional(),
+    SALT_ROUNDS: z.coerce.number(),
+    NEXTAUTH_URL: z.string().url(),
+    NEXTAUTH_SECRET: z.string().min(1),
+  },
+  experimental__runtimeEnv: {},
 });
-
-export const serverEnv = serverEnvSchema.parse(process.env);
