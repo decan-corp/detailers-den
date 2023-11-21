8308940f6de9d0c802b992c244c4d007e6ed9441
Emman Nipal <emnnipal@gmail.com>
2023-11-21 22:34:27 +0800

    feat: add loading in admin page routes

--- src/app/(admin-pages)/auth/login/page.tsx
+++ src/app/(admin-pages)/auth/login/page.tsx
@@ -6,22 +6,6 @@ export const metadata: Metadata = {
   title: 'Login',
 };
 
-// const Login = () => (
-//   <main className="hero min-h-screen bg-base-200">
-//     <div className="hero-content flex-col-reverse gap-6 lg:flex-row-reverse">
-//       <LoginForm />
-//       <div className="flex max-w-xl flex-col items-center text-center lg:items-end lg:text-right">
-//         <AppLogo className="w-52" />
-//         <p className="py-6">Welcome to 185 Detailers Den Admin Portal</p>
-//         <p className="hidden lg:block">
-//           Login using the provided credentials to manage and oversee administrative tasks with
-//           efficiency.
-//         </p>
-//       </div>
-//     </div>
-//   </main>
-// );
-
 const Login = () => (
   <main className="grid min-h-screen lg:grid-cols-2">
     <div className="hidden flex-col justify-between bg-muted p-10 lg:flex">
