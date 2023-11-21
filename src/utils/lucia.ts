636bf4ba99cae5454027e85169c583873001cd98
Emman Nipal <emnnipal@gmail.com>
2023-11-21 22:35:01 +0800

    fix: change lucia session expires config

--- src/utils/lucia.ts
+++ src/utils/lucia.ts
@@ -3,8 +3,12 @@ import 'lucia/polyfill/node';
 import { pool } from './db';
 
 import { mysql2 } from '@lucia-auth/adapter-mysql';
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
 import { lucia } from 'lucia';
 import { nextjs_future } from 'lucia/middleware';
+
+dayjs.extend(duration);
 
 export const auth = lucia({
   adapter: mysql2(pool, {
@@ -16,6 +20,10 @@ export const auth = lucia({
   middleware: nextjs_future(),
   sessionCookie: {
     expires: false,
+  },
+  sessionExpiresIn: {
+    activePeriod: dayjs.duration(1, 'days').as('milliseconds'),
+    idlePeriod: dayjs.duration(7, 'days').as('milliseconds'),
   },
   getUserAttributes: (data) => ({
     email: data.email,
