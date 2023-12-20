a32cbb461b31fb95fff984777d63f58b6bcaae28
Emman Nipal <emnnipal@gmail.com>
2023-12-21 01:15:26 +0800

    fix: too many connections issue when in local development

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,10 +1,17 @@
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
 import { serverEnv } from 'src/env/server';
 import * as schema from 'src/schema';
 
-import { drizzle } from 'drizzle-orm/mysql2';
-import { PoolOptions, createPool } from 'mysql2/promise';
+import { MySql2Database, drizzle } from 'drizzle-orm/mysql2';
+import { PoolOptions, createPool, Pool } from 'mysql2/promise';
 
 let poolOptions: PoolOptions;
+
+declare global {
+  var db: undefined | MySql2Database<typeof schema>;
+  var pool: undefined | Pool;
+}
 
 if (serverEnv.DB_HOST !== 'localhost') {
   poolOptions = {
@@ -24,6 +31,11 @@ if (serverEnv.DB_HOST !== 'localhost') {
   };
 }
 
-export const pool = createPool(poolOptions);
+export const pool = globalThis.pool ?? createPool(poolOptions);
 
-export const db = drizzle(pool, { schema, mode: 'planetscale' });
+export const db = globalThis.db ?? drizzle(pool, { schema, mode: 'planetscale' });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.db = db;
+  globalThis.pool = pool;
+}
