b69fef7968528e0ba21219daea9e36b9d2830eac
Emman Nipal <emnnipal@gmail.com>
2024-01-01 01:05:33 +0800

    feat: enhance view transaction UI with cards component

--- src/app/(admin-pages)/admin/pos/[transactionId]/page.tsx
+++ src/app/(admin-pages)/admin/pos/[transactionId]/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Icons } from '@/components/ui/icons';
-import { Separator } from '@/components/ui/separator';
+import { Label } from '@/components/ui/label';
 import {
   Table,
   TableBody,
@@ -55,136 +55,142 @@ const ViewTransaction = ({ params }: { params: { transactionId: string } }) => {
   }
 
   return (
-    <div className="space-y-32 bg-background p-4 sm:p-10 sm:px-12">
-      <div className="">
+    <div className="space-y-16 bg-background p-4 sm:p-10 sm:px-12">
+      <div className="space-y-6">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Transaction Details</h2>
           <p className="text-muted-foreground">{transaction.plateNumber}</p>
         </div>
-        <Separator className="my-6" />
-        <div className="grid grid-cols-4 gap-20 space-y-6 sm:grid-cols-6">
-          <div className="col-span-2 flex flex-col gap-7">
-            <div className="space-y-2">
-              <CardTitle>Customer Name</CardTitle>
-              <div className="mt-2 text-muted-foreground">{transaction.customerName}</div>
-            </div>
-
-            <div className="space-y-2">
-              <CardTitle>Status</CardTitle>
-              <div className="mt-2 text-muted-foreground">{transaction.status}</div>
-            </div>
-
-            <div className="space-y-2">
-              <CardTitle>Vehicle Size</CardTitle>
-              <div className="mt-2 text-muted-foreground">{transaction.vehicleSize}</div>
-            </div>
-
-            <div className="space-y-2">
-              <CardTitle>Mode of Payment</CardTitle>
-              <div className="mt-2 text-muted-foreground">{transaction.modeOfPayment}</div>
-            </div>
-          </div>
-          <div className="col-span-2 flex flex-col gap-7">
-            <div className="space-y-2">
-              <CardTitle>Note</CardTitle>
-              <div className="mt-2 text-muted-foreground">{transaction.note}</div>
-            </div>
-
-            <div className="space-y-2">
-              <CardTitle>Discount</CardTitle>
-              <div className="mt-2 text-muted-foreground">{transaction.discount}</div>
-            </div>
-
-            <div className="space-y-2">
-              <CardTitle>Tip</CardTitle>
-              <div className="mt-2 text-muted-foreground">{transaction.tip}</div>
-            </div>
-
-            <div className="space-y-2">
-              <CardTitle>Total Price</CardTitle>
-              <div className="mt-2 text-muted-foreground">{transaction.totalPrice}</div>
-            </div>
-          </div>
-          <div className="col-span-2 flex flex-col gap-7">
-            <div className="space-y-2">
-              <CardTitle>Created At</CardTitle>
-              <div className="mt-2 text-muted-foreground">
-                {dayjs(transaction.createdAt).format('MMM DD, YYYY hh:mm:a') || 'n/a'}
+        <Card>
+          <CardContent className="grid grid-cols-4 gap-y-10 pt-6 sm:grid-cols-6">
+            <div className="col-span-2 flex flex-col gap-6">
+              <div className="space-y-1">
+                <Label>Customer Name</Label>
+                <div className="mt-2 text-sm text-muted-foreground">{transaction.customerName}</div>
+              </div>
+              <div className="space-y-1">
+                <Label>Status</Label>
+                <div className="mt-2 text-sm text-muted-foreground">{transaction.status}</div>
+              </div>
+              <div className="space-y-1">
+                <Label>Vehicle Size</Label>
+                <div className="mt-2 text-sm text-muted-foreground">{transaction.vehicleSize}</div>
+              </div>
+              <div className="space-y-1">
+                <Label>Mode of Payment</Label>
+                <div className="mt-2 text-sm text-muted-foreground">
+                  {transaction.modeOfPayment}
+                </div>
               </div>
             </div>
-
-            <div className="space-y-2">
-              <CardTitle>Updated At</CardTitle>
-              <div className="mt-2 text-muted-foreground">
-                {transaction.updatedAt
-                  ? dayjs(transaction.updatedAt).format('MMM DD, YYYY hh:mm:a')
-                  : ''}
+            <div className="col-span-2 flex flex-col gap-6">
+              <div className="space-y-1">
+                <Label>Note</Label>
+                <div className="mt-2 text-sm text-muted-foreground">{transaction.note}</div>
+              </div>
+              <div className="space-y-1">
+                <Label>Discount</Label>
+                <div className="mt-2 text-sm text-muted-foreground">{transaction.discount}</div>
+              </div>
+              <div className="space-y-1">
+                <Label>Customer Name</Label>
+                <div className="mt-2 text-sm text-muted-foreground">{transaction.customerName}</div>
+              </div>
+              <div className="space-y-1">
+                <Label>Total Price</Label>
+                <div className="mt-2 text-sm text-muted-foreground">
+                  Php {transaction.totalPrice}
+                </div>
               </div>
             </div>
-            <div className="space-y-2">
-              <CardTitle>Completed At</CardTitle>
-              <div className="mt-2 text-muted-foreground">
-                {transaction.completedAt
-                  ? dayjs(transaction.completedAt).format('MMM DD, YYYY hh:mm:a')
-                  : ''}
+            <div className="col-span-2 flex flex-col gap-6">
+              <div className="space-y-1">
+                <Label>Created At</Label>
+                <div className="mt-2 text-sm text-muted-foreground">
+                  {dayjs(transaction.createdAt).format('MMM DD, YYYY hh:mm:a') || 'n/a'}
+                </div>
+              </div>
+              <div className="space-y-1">
+                <Label>Updated At</Label>
+                <div className="mt-2 text-sm text-muted-foreground">
+                  {transaction.updatedAt
+                    ? dayjs(transaction.updatedAt).format('MMM DD, YYYY hh:mm:a')
+                    : ''}
+                </div>
+              </div>
+              <div className="space-y-1">
+                <Label>Completed At</Label>
+                <div className="mt-2 text-sm text-muted-foreground">
+                  {transaction.completedAt
+                    ? dayjs(transaction.completedAt).format('MMM DD, YYYY hh:mm:a')
+                    : ''}
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          </CardContent>
+        </Card>
       </div>
 
-      <div>
+      <div className="space-y-6">
         <div className="my-6">
-          <h2 className="text-2xl font-bold tracking-tight">Services</h2>
+          <h2 className="text-2xl font-bold tracking-tight">Availed Services</h2>
         </div>
         <div className="space-y-10">
           {transactionServices?.map((transactionService, index) => (
-            <div key={transactionService.id} className="space-y-10">
-              <CardTitle>Service #{index + 1}</CardTitle>
-              <div className="flex flex-row gap-20">
-                <div className="space-y-2">
-                  <CardTitle>Service Name</CardTitle>
-                  <div className="mt-2 text-muted-foreground">{transactionService.serviceName}</div>
-                </div>
-
-                <div className="space-y-2">
-                  <CardTitle>Price</CardTitle>
-                  <div className="mt-2 text-muted-foreground">{transactionService.price}</div>
-                </div>
-                <div className="space-y-2">
-                  <CardTitle>Service Cut % Modifier</CardTitle>
-                  <div className="mt-2 text-muted-foreground">
-                    {transactionService.serviceCutPercentage}%
+            <Card key={transactionService.id} className="">
+              <CardHeader>
+                <CardTitle>Service #{index + 1}</CardTitle>
+              </CardHeader>
+              <CardContent className="space-y-6">
+                <div className="flex flex-col gap-x-32 gap-y-6 py-4 sm:flex-row">
+                  <div className="space-y-1">
+                    <Label>Service Name</Label>
+                    <div className="mt-2 text-sm text-muted-foreground">
+                      {transactionService.serviceName}
+                    </div>
+                  </div>
+                  <div className="space-y-1">
+                    <Label>Price</Label>
+                    <div className="mt-2 text-sm text-muted-foreground">
+                      {transactionService.price}
+                    </div>
+                  </div>
+                  <div className="space-y-1">
+                    <Label>Service Cut % Modifier</Label>
+                    <div className="mt-2 text-sm text-muted-foreground">
+                      {transactionService.serviceCutPercentage}%
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="mt-2 space-y-2">
-                <CardTitle>Crew Earnings</CardTitle>
-                <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead className="w-[100px]">Crew Name</TableHead>
-                      <TableHead>Role</TableHead>
-                      <TableHead>Crew Service Cut %</TableHead>
-                      <TableHead>Computed Cut %</TableHead>
-                      <TableHead className="text-right">Amount Earned</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    {transactionService.crewEarnings.map((crewEarning) => (
-                      <TableRow key={crewEarning.id}>
-                        <TableCell className="font-medium">{crewEarning.crewName}</TableCell>
-                        <TableCell>{crewEarning.role}</TableCell>
-                        <TableCell>{crewEarning.crewServiceCutPercentage}%</TableCell>
-                        <TableCell>{crewEarning.computedServiceCutPercentage}%</TableCell>
-                        <TableCell className="text-right">PHP {crewEarning.amountEarned}</TableCell>
+                <div className="mt-2 space-y-2">
+                  <CardTitle>Crew Earnings</CardTitle>
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="w-[100px]">Crew Name</TableHead>
+                        <TableHead>Role</TableHead>
+                        <TableHead>Crew Service Cut %</TableHead>
+                        <TableHead>Computed Cut %</TableHead>
+                        <TableHead className="text-right">Amount Earned</TableHead>
                       </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </div>
-              <Separator />
-            </div>
+                    </TableHeader>
+                    <TableBody>
+                      {transactionService.crewEarnings.map((crewEarning) => (
+                        <TableRow key={crewEarning.id}>
+                          <TableCell className="font-medium">{crewEarning.crewName}</TableCell>
+                          <TableCell>{crewEarning.role}</TableCell>
+                          <TableCell>{crewEarning.crewServiceCutPercentage}%</TableCell>
+                          <TableCell>{crewEarning.computedServiceCutPercentage}%</TableCell>
+                          <TableCell className="text-right">
+                            PHP {crewEarning.amountEarned}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </div>
+              </CardContent>
+            </Card>
           ))}
         </div>
       </div>
