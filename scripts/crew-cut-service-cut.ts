9f1a63102ae8ba75da72ef9aa6ed7a441c695d8e
Emman Nipal <emnnipal@gmail.com>
2024-02-04 01:52:25 +0800

    chore: do not use transactions on long running migrations

--- scripts/crew-cut-service-cut.ts
+++ scripts/crew-cut-service-cut.ts
@@ -9,51 +9,47 @@ import { db } from 'src/utils/db';
 import { eq } from 'drizzle-orm';
 
 const seedDatabase = async () => {
-  await db.transaction(async (tx) => {
-    const users = await tx.select().from(usersTable);
-    const crewEarnings = await tx.select().from(crewEarningsTable);
+  const users = await db.select().from(usersTable);
+  const crewEarnings = await db.select().from(crewEarningsTable);
 
-    for (const crewEarning of crewEarnings) {
-      if (!crewEarning.crewCutPercentage) {
-        const crew = users.find(({ id }) => id === crewEarning.crewId);
+  for (const crewEarning of crewEarnings) {
+    if (!crewEarning.crewCutPercentage) {
+      const crew = users.find(({ id }) => id === crewEarning.crewId);
 
-        if (crew) {
-          await tx
-            .update(crewEarningsTable)
-            .set({
-              crewCutPercentage: crew.serviceCutPercentage || 0,
-            })
-            .where(eq(crewEarningsTable.id, crewEarning.id));
-        }
+      if (crew) {
+        await db
+          .update(crewEarningsTable)
+          .set({
+            crewCutPercentage: crew.serviceCutPercentage || 0,
+          })
+          .where(eq(crewEarningsTable.id, crewEarning.id));
       }
     }
-  });
+  }
 
   console.log('Success seeding crewCutPercentage');
   console.log('Starting seeding service cut percentage of transactionServices');
 
-  await db.transaction(async (tx) => {
-    const transactionServices = await tx.select().from(transactionServicesTable);
-    const crewEarningsRef = await tx.select().from(crewEarningsTable);
+  const transactionServices = await db.select().from(transactionServicesTable);
+  const crewEarningsRef = await db.select().from(crewEarningsTable);
 
-    for (const transactionService of transactionServices) {
-      const crewEarnings = crewEarningsRef.filter(
-        ({ transactionServiceId }) => transactionServiceId === transactionService.id
-      );
-      const [crewEarning] = crewEarnings;
+  for (const transactionService of transactionServices) {
+    const modifiedCrewEarnings = crewEarningsRef.filter(
+      ({ transactionServiceId }) => transactionServiceId === transactionService.id
+    );
+    const [crewEarning] = modifiedCrewEarnings;
 
-      if (crewEarning) {
-        await tx
-          .update(transactionServicesTable)
-          .set({
-            serviceCutPercentage:
-              (crewEarning.computedServiceCutPercentage || 0) * crewEarnings.length -
-              crewEarning.crewCutPercentage,
-          })
-          .where(eq(transactionServicesTable.id, transactionService.id));
-      }
+    if (crewEarning) {
+      await db
+        .update(transactionServicesTable)
+        .set({
+          serviceCutPercentage:
+            (crewEarning.computedServiceCutPercentage || 0) * modifiedCrewEarnings.length -
+            crewEarning.crewCutPercentage,
+        })
+        .where(eq(transactionServicesTable.id, transactionService.id));
     }
-  });
+  }
 };
 
 seedDatabase()
