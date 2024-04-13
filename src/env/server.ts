1160dc874160d78c19c8ead780df40acc04281f4
Emman Nipal <emnnipal@gmail.com>
2024-04-14 07:46:21 +0800

    fix: unable to read environment variable for db migrate script

--- src/env/server.ts
+++ src/env/server.ts
@@ -3,12 +3,11 @@ import { z } from 'zod';
 
 export const serverEnv = createEnv({
   server: {
-    DB_HOST: z.string().min(1),
-    DB_USERNAME: z.string().min(1),
-    DB_PASSWORD: z.string().min(1),
-    DB_NAME: z.string().optional(), // used for db connection in local development
     TURSO_DATABASE_URL: z.string().min(1),
     TURSO_AUTH_TOKEN: z.string().min(1),
   },
-  experimental__runtimeEnv: {},
+  runtimeEnv: {
+    TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
+    TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
+  },
 });
