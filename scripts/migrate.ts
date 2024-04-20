506eadd54a003fa3dcecc23ed3456347a4035e7d
Emman Nipal <emnnipal@gmail.com>
2024-04-20 16:08:05 +0800

    chore: add dotenv in migrate script

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -4,9 +4,12 @@
 import { serverEnv } from 'src/env/server';
 import { db } from 'src/utils/db';
 
+import dotenv from 'dotenv';
 import { migrate } from 'drizzle-orm/libsql/migrator';
 
 import path from 'path';
+
+dotenv.config();
 
 // TODO: create script for sqlite local
 (async () => {
