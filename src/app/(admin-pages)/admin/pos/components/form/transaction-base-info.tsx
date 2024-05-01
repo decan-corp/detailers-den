7297d2ffa6035a9d6dd3486ec83647a9b57c5f61
Emman Nipal <emnnipal@gmail.com>
2024-05-01 15:13:54 +0800

    fix: service price field not updating when changing vehicle size

--- src/app/(admin-pages)/admin/pos/components/form/transaction-base-info.tsx
+++ src/app/(admin-pages)/admin/pos/components/form/transaction-base-info.tsx
@@ -10,6 +10,7 @@ import {
 import { Textarea } from '@/components/ui/textarea';
 import { RequiredIndicatorIcon } from 'src/components/form/required-indicator';
 import { ModeOfPayment, TransactionStatus, VehicleSize } from 'src/constants/common';
+import { useServiceOptions } from 'src/queries/services';
 import { transactionSchema } from 'src/schemas/transactions';
 
 import {
@@ -18,198 +19,243 @@ import {
   vehicleSizeOptions,
 } from '../data-table-options';
 
-import { UseFormReturn } from 'react-hook-form';
+import { useMemo } from 'react';
+import { UseFormReturn, useWatch } from 'react-hook-form';
 import { z } from 'zod';
 
 const TransactionBaseInfo = ({
   form,
 }: {
   form: UseFormReturn<z.input<typeof transactionSchema>>;
-}) => (
-  <div className="space-y-4">
-    <FormField
-      control={form.control}
-      name="customerName"
-      render={({ field }) => (
-        <FormItem className="">
-          <FormLabel>Customer Name</FormLabel>
-          <FormControl>
-            <Input {...field} value={field.value || ''} />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-    <div className="grid grid-cols-2 gap-4">
+}) => {
+  const watchedData = useWatch();
+  const { data: serviceOptions = [] } = useServiceOptions();
+
+  const formState = useMemo(
+    () => ({
+      ...watchedData,
+      ...form.getValues(),
+    }),
+    [form, watchedData]
+  );
+
+  const onChangeVehicleSize = (selectedVehicleSize: VehicleSize) => {
+    form.setValue('vehicleSize', selectedVehicleSize);
+
+    const transactionServices = [...formState.transactionServices];
+
+    const state = transactionServices
+      .filter(({ serviceId }) => {
+        if (serviceId === '') return true;
+
+        const service = serviceOptions.find(({ id }) => id === serviceId);
+        const priceMatrix = service?.priceMatrix.find(
+          ({ vehicleSize }) => vehicleSize === selectedVehicleSize
+        );
+
+        return !!priceMatrix;
+      })
+      .map((data) => {
+        if (data.serviceId === '') return data;
+
+        const service = serviceOptions.find(({ id }) => id === data.serviceId);
+        const priceMatrix = service?.priceMatrix.find(
+          ({ vehicleSize }) => vehicleSize === selectedVehicleSize
+        );
+
+        if (!service || !priceMatrix) return data;
+
+        return {
+          ...data,
+          price: priceMatrix.price,
+        };
+      });
+
+    form.setValue('transactionServices', state);
+  };
+  return (
+    <div className="space-y-4">
       <FormField
         control={form.control}
-        name="plateNumber"
+        name="customerName"
         render={({ field }) => (
           <FormItem className="">
-            <FormLabel className="relative">
-              Plate Number <RequiredIndicatorIcon />
-            </FormLabel>
+            <FormLabel>Customer Name</FormLabel>
             <FormControl>
-              <Input
-                {...field}
-                value={field.value || ''}
-                minLength={6}
-                maxLength={12}
-                onKeyDown={(e) => {
-                  if (e.code === 'Space') {
-                    e.preventDefault();
-                  }
-                }}
-                onChange={(e) => {
-                  field.onChange(e.currentTarget.value.toUpperCase());
-                }}
-              />
+              <Input {...field} value={field.value || ''} />
             </FormControl>
             <FormMessage />
           </FormItem>
         )}
       />
-
-      <FormField
-        control={form.control}
-        name="vehicleSize"
-        render={({ field }) => (
-          <FormItem className="w-full ">
-            <FormLabel className="relative">
-              Vehicle Size <RequiredIndicatorIcon />
-            </FormLabel>
-            <Select
-              onValueChange={(value) => form.setValue('vehicleSize', value as VehicleSize)}
-              value={field.value}
-            >
+      <div className="grid grid-cols-2 gap-4">
+        <FormField
+          control={form.control}
+          name="plateNumber"
+          render={({ field }) => (
+            <FormItem className="">
+              <FormLabel className="relative">
+                Plate Number <RequiredIndicatorIcon />
+              </FormLabel>
               <FormControl>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select vehicle size..." />
-                </SelectTrigger>
+                <Input
+                  {...field}
+                  value={field.value || ''}
+                  minLength={6}
+                  maxLength={12}
+                  onKeyDown={(e) => {
+                    if (e.code === 'Space') {
+                      e.preventDefault();
+                    }
+                  }}
+                  onChange={(e) => {
+                    field.onChange(e.currentTarget.value.toUpperCase());
+                  }}
+                />
               </FormControl>
-              <SelectContent>
-                {vehicleSizeOptions.map(({ value, label, icon: Icon }) => (
-                  <SelectItem key={value} value={value}>
-                    <div className="flex flex-row items-center gap-3">
-                      <Icon className="h-4 w-4 text-muted-foreground" /> {label}
-                    </div>
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-    </div>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-    <div className="grid grid-cols-2 gap-4">
-      <FormField
-        control={form.control}
-        name="status"
-        render={({ field }) => (
-          <FormItem className="w-full ">
-            <FormLabel className="relative">
-              Status <RequiredIndicatorIcon />
-            </FormLabel>
-            <Select
-              onValueChange={(value) => form.setValue('status', value as TransactionStatus)}
-              value={field.value}
-            >
+        <FormField
+          control={form.control}
+          name="vehicleSize"
+          render={({ field }) => (
+            <FormItem className="w-full ">
+              <FormLabel className="relative">
+                Vehicle Size <RequiredIndicatorIcon />
+              </FormLabel>
+              <Select onValueChange={onChangeVehicleSize} value={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select vehicle size..." />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {vehicleSizeOptions.map(({ value, label, icon: Icon }) => (
+                    <SelectItem key={value} value={value}>
+                      <div className="flex flex-row items-center gap-3">
+                        <Icon className="h-4 w-4 text-muted-foreground" /> {label}
+                      </div>
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+
+      <div className="grid grid-cols-2 gap-4">
+        <FormField
+          control={form.control}
+          name="status"
+          render={({ field }) => (
+            <FormItem className="w-full ">
+              <FormLabel className="relative">
+                Status <RequiredIndicatorIcon />
+              </FormLabel>
+              <Select
+                onValueChange={(value) => form.setValue('status', value as TransactionStatus)}
+                value={field.value}
+              >
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select status..." />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {transactionStatusOptions.map(({ value, label }) => (
+                    <SelectItem key={value} value={value}>
+                      {label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="modeOfPayment"
+          render={({ field }) => (
+            <FormItem className="w-full ">
+              <FormLabel className="relative">
+                Mode of Payment <RequiredIndicatorIcon />
+              </FormLabel>
+              <Select
+                onValueChange={(value) => form.setValue('modeOfPayment', value as ModeOfPayment)}
+                value={field.value}
+              >
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select mode..." />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {modeOfPaymentOptions.map(({ value, label, icon: Icon }) => (
+                    <SelectItem key={value} value={value}>
+                      <div className="flex flex-row items-center gap-3">
+                        <Icon className="h-4 w-4 text-muted-foreground" /> {label}
+                      </div>
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+
+      <div className="grid grid-cols-2 gap-4">
+        <FormField
+          control={form.control}
+          name="discount"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Discount</FormLabel>
               <FormControl>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select status..." />
-                </SelectTrigger>
+                <Input type="number" min={0} step={0.01} {...field} value={field.value || 0} />
               </FormControl>
-              <SelectContent>
-                {transactionStatusOptions.map(({ value, label }) => (
-                  <SelectItem key={value} value={value}>
-                    {label}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-
-      <FormField
-        control={form.control}
-        name="modeOfPayment"
-        render={({ field }) => (
-          <FormItem className="w-full ">
-            <FormLabel className="relative">
-              Mode of Payment <RequiredIndicatorIcon />
-            </FormLabel>
-            <Select
-              onValueChange={(value) => form.setValue('modeOfPayment', value as ModeOfPayment)}
-              value={field.value}
-            >
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="tip"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Tip</FormLabel>
               <FormControl>
-                <SelectTrigger>
-                  <SelectValue placeholder="Select mode..." />
-                </SelectTrigger>
+                <Input type="number" min={0} step={0.01} {...field} value={field.value || 0} />
               </FormControl>
-              <SelectContent>
-                {modeOfPaymentOptions.map(({ value, label, icon: Icon }) => (
-                  <SelectItem key={value} value={value}>
-                    <div className="flex flex-row items-center gap-3">
-                      <Icon className="h-4 w-4 text-muted-foreground" /> {label}
-                    </div>
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-    </div>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
 
-    <div className="grid grid-cols-2 gap-4">
       <FormField
         control={form.control}
-        name="discount"
+        name="note"
         render={({ field }) => (
           <FormItem>
-            <FormLabel>Discount</FormLabel>
+            <FormLabel>Note</FormLabel>
             <FormControl>
-              <Input type="number" min={0} step={0.01} {...field} value={field.value || 0} />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <FormField
-        control={form.control}
-        name="tip"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Tip</FormLabel>
-            <FormControl>
-              <Input type="number" min={0} step={0.01} {...field} value={field.value || 0} />
+              <Textarea {...field} className="[field-sizing:content]" value={field.value || ''} />
             </FormControl>
             <FormMessage />
           </FormItem>
         )}
       />
     </div>
-
-    <FormField
-      control={form.control}
-      name="note"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>Note</FormLabel>
-          <FormControl>
-            <Textarea {...field} className="[field-sizing:content]" value={field.value || ''} />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-  </div>
-);
+  );
+};
 
 export default TransactionBaseInfo;
